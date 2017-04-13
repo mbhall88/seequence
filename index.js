1fa27c421e49677dc08b59c3f08eb4a258bd16ee
Michael Hall <mbhall88@gmail.com>
2017-04-13 15:44:20 +1000

    add anoter pane, now three panes. zoom and brushing working on all three.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function parallelCoordinates() {
     var data          = [],
 	    width,
-	    margin        = {top: 20, right: 30, bottom: 110, left: 40},
-	    margin2       = {top: 430, right: 20, bottom: 30, left: 40}, // margin for context pane
-        height,
+	    marginTop        = {top: 20, right: 30, bottom: 250, left: 40}, // margin for sequence pane
+	    marginBottom       = {top: 430, right: 20, bottom: 30, left: 40}, // margin for context pane
+	    marginMiddle       = {top: 290, right: 30, bottom: 110, left: 40}, // margin for entropy pane
+        heightTop,
         domain        = ["A", "T", "C", "G", "-"], // y domain
         mapping       = [], // data that maps the sample id to MIC, sequence etc
         entropy, // array of entropy for each position
@@ -20,13 +21,13 @@ function parallelCoordinates() {
 
 	//=======================================================================
 	// SCALES FOR THE AXES - 2 indicates use for context pane
-     var x = d3.scaleLinear(),
-         x2 = d3.scaleLinear(),
+     var xTop = d3.scaleLinear(),
+         xBottom = d3.scaleLinear(),
          // padding makes lines the points up with the y axis ticks
-         y = d3.scaleBand().paddingInner(1).paddingOuter(0.25),
-         y2 = d3.scaleBand().paddingInner(1).paddingOuter(0.25),
+         yTop = d3.scaleBand().paddingInner(1).paddingOuter(0.25),
+         yBottom = d3.scaleBand().paddingInner(1).paddingOuter(0.25),
 		 yEntropy = d3.scaleLinear(),
-		 yEntropy2 = d3.scaleLinear();
+		 yEntropyBottom = d3.scaleLinear();
 	//=======================================================================
 
 
@@ -36,17 +37,17 @@ function parallelCoordinates() {
     var line = function(d) {
 	    return d3.line()
 		    .curve(d3.curveMonotoneY)
-		    .x(function (d) { return x(d.position); })
-		    .y(function (d) { return y(d.bases); })
+		    .x(function (d) { return xTop(d.position); })
+		    .y(function (d) { return yTop(d.bases); })
 		    (d.values);
     };
 
     // used for lines in the context pane
-    var line2 = function(d) {
+    var lineBottom = function(d) {
 	    return d3.line()
 		    // .curve(d3.curveStep)
-		    .x(function (d) { return x2(d.position); })
-		    .y(function (d) { return y2(d.bases); })
+		    .x(function (d) { return xBottom(d.position); })
+		    .y(function (d) { return yBottom(d.bases); })
 		    (d.values);
     };
 
@@ -54,16 +55,16 @@ function parallelCoordinates() {
 	var entropyLine = function(d, i) {
 		return d3.line()
 			.curve(d3.curveBasis)
-			.x(function (d, i) { return x(i + 1); })
+			.x(function (d, i) { return xTop(i + 1); })
 			.y(function (d) { return yEntropy(d); })
 			(d, i);
 	};
 
-	var entropyLine2 = function(d, i) {
+	var entropyLineBottom = function(d, i) {
 		return d3.line()
 			.curve(d3.curveBasis)
-			.x(function (d, i) { return x2(i + 1); })
-			.y(function (d) { return yEntropy2(d); })
+			.x(function (d, i) { return xBottom(i + 1); })
+			.y(function (d) { return yEntropyBottom(d); })
 			(d, i);
 	};
 
@@ -77,15 +78,17 @@ function parallelCoordinates() {
 	        //========================================================================
 	        // INITIALISE VARIABLES AND SVG ELEMENTS
 
-	        var height2   = totalHeight - margin2.top - margin2.bottom, // height for context pane
-	            seqLength = data[0].values.length;
+	        var heightBottom   = totalHeight - marginBottom.top - marginBottom.bottom, // height for context pane
+	            heightMiddle   = totalHeight - marginMiddle.top - marginMiddle.bottom,
+	            focusHeight    = totalHeight - marginTop.top - marginMiddle.bottom,
+	            seqLength      = data[0].values.length;
 
-	        x.domain([1, seqLength]).range([0, width]);
-	        x2.domain([1, seqLength]).range([0, width]);
-	        y.domain(domain).range([height, 0]);
-	        y2.domain(domain).range([height2, 0]);
-	        yEntropy.domain(d3.extent(entropy)).range([height, 0]);
-	        yEntropy2.domain(d3.extent(entropy)).range([height2, 0]);
+	        xTop.domain([1, seqLength]).range([0, width]);
+	        xBottom.domain([1, seqLength]).range([0, width]);
+	        yTop.domain(domain).range([heightTop, 0]);
+	        yBottom.domain(domain).range([heightBottom, 0]);
+	        yEntropy.domain(d3.extent(entropy)).range([heightMiddle, 0]);
+	        yEntropyBottom.domain(d3.extent(entropy)).range([heightBottom, 0]);
 
 	        // colour each line based on the colourBy specified
 	        if (lineColourBy) {
@@ -93,13 +96,13 @@ function parallelCoordinates() {
 	        }
 
 	        var brush = d3.brushX() // limits brushing to x axis
-		        .extent([[0,0], [width, height2]]) // brushing only in context pane
+		        .extent([[0,0], [width, heightBottom]]) // brushing only in context pane
 		        .on('brush end', brushed); // when brushing or when brushing ends call brushed
 
 	        var zoom = d3.zoom()
-		        .scaleExtent([1, seqLength / baseResolution]) // scale from 1x to Infinity
-		        .translateExtent([[0,0], [width, height]]) // cannot pan outside focus pane
-		        .extent([[0, 0], [width, height]]) // only zoom within focus pane
+		        .scaleExtent([1, seqLength / baseResolution]) // scale from 1 to ...
+		        .translateExtent([[0,0], [width, focusHeight]]) // cannot pan outside focus pane
+		        .extent([[0, 0], [width, focusHeight]]) // only zoom within focus pane
 		        .on('zoom', zoomed);
 
             // append the svg object to the selection
@@ -112,25 +115,29 @@ function parallelCoordinates() {
 		        .attr('id', 'clip')
 		        .append('rect')
 		        .attr('width', width)
-		        .attr('height', height);
+		        .attr('height', focusHeight);
 
             // this will be the area showing the brushed/zoomed segment of the data
-            var focus = svg.append('g')
-	            .attr('class', 'focus')
-	            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+            var sequencePane = svg.append('g')
+	            .attr('class', 'sequencePane')
+	            .attr('transform', 'translate(' + marginTop.left + ',' + marginTop.top + ')');
+
+            var entropyPane = svg.append('g')
+	            .attr('class', 'entropyPane')
+	            .attr('transform', 'translate(' + marginMiddle.left + ',' + marginMiddle.top + ')');
 
             // this will be the area showing all of the data (think of it as fully zoomed out)
 	        var context = svg.append('g')
 		        .attr('class', 'context')
-		        .attr('transform', 'translate(' + margin2.left + ',' + margin2.top + ')');
+		        .attr('transform', 'translate(' + marginBottom.left + ',' + marginBottom.top + ')');
 
 	        // transparent window over focus that allows zooming. THIS MUST COME AFTER THE
 	        // FOCUS AND CONTEXT PANES HAVE BEEN APPENDED
 	        svg.append('rect')
 		        .attr('class', 'zoom')
 		        .attr('width', width)
-		        .attr('height', height)
-		        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+		        .attr('height', focusHeight)
+		        .attr('transform', 'translate(' + marginTop.left + ',' + marginTop.top + ')')
 		        .call(zoom);
 	        //========================================================================
 
@@ -140,50 +147,83 @@ function parallelCoordinates() {
 
 	        // defining the axes
 	        // xAxisTop will draw vertical lines that stay aligned with the plot when panning
-	        var xAxisTop = d3.axisTop(x).ticks(baseResolution).tickSize(height, 0),
-	            xAxisBottom = d3.axisBottom(x),
-	            xAxis2 = d3.axisBottom(x2),
-		        yAxisLeft = d3.axisLeft(y),
-		        yAxisRight = d3.axisRight(y);
+	        var xAxisTopSeq = d3.axisTop(xTop).ticks(baseResolution).tickSize(heightTop, 0),
+	            xAxisBottomFocus = d3.axisBottom(xTop),
+	            xAxisContext = d3.axisBottom(xBottom),
+		        yAxisLeftSeq = d3.axisLeft(yTop),
+		        yAxisRightSeq = d3.axisRight(yTop),
+                yAxisEntropy  = d3.axisLeft(yEntropy).ticks(5);
 
-            // add x axes to focus
-            focus.append('g')
+            // add x axes to sequence pane
+            sequencePane.append('g')
 	            .attr('class', 'x axis top')
-	            .attr('transform', 'translate(0,' + height + ')')
-	            .call(xAxisTop);
+	            .attr('transform', 'translate(0,' + heightTop + ')')
+	            .call(xAxisTopSeq);
 
-	        focus.append('g')
+	        sequencePane.append('g')
 		        .attr('class', 'x axis bottom')
-		        .attr('transform', 'translate(0,' + height + ')')
-		        .call(xAxisBottom);
+		        .attr('transform', 'translate(0,' + heightTop + ')')
+		        .call(xAxisBottomFocus);
 
-            // add y axis to left and right in focus
-            focus.append('g')
+            // add y axis to left and right in sequence pane
+            sequencePane.append('g')
 	            .attr('class', 'y axis left')
-	            .call(yAxisLeft);
+	            .call(yAxisLeftSeq);
 
-	        focus.append('g')
+	        sequencePane.append('g')
 		        .attr('class', 'y axis right')
 		        .attr('transform', 'translate(' + width + ',0)')
-		        .call(yAxisRight);
+		        .call(yAxisRightSeq);
 
-	        // add the Y gridlines
-	        focus.append("g")
+	        // add the Y gridlines to sequence pane
+	        sequencePane.append("g")
 		        .attr("class", "grid")
-		        .call(make_y_gridlines().tickSize(-width).tickFormat(""));
+		        .call(make_y_gridlines(yTop).tickSize(-width).tickFormat(""));
+
+	        // add entropy x axis
+	        entropyPane.append('g')
+		        .attr('class', 'x axis entropy')
+		        .attr('transform', 'translate(0,' + heightMiddle + ')')
+		        .call(xAxisBottomFocus);
+
+	        // add entropy y axis
+	        entropyPane.append('g')
+		        .attr('class', 'y axis entropy')
+		        .call(yAxisEntropy);
+
+	        // make entropy y gridline
+	        entropyPane.append("g")
+		        .attr("class", "grid")
+		        .call(make_y_gridlines(yEntropy).tickSize(-width).tickFormat(""));
+
+	        // var entropyContainer = entropyPane.selectAll('.entropyContainer')
+				// .datum(entropy)
+			 //  .enter().append('path')
+				// .attr('class', 'entropyContainer')
+				// .attr('d', entropyLine);
+	        //
+	        // entropyContainer.append('path')
+		     //    .attr('class', 'entropy')
+		     //    .attr('d', entropyLine)
+		     //    .attr('stroke', 'green')
+		     //    .attr('stroke-width', '2px')
+		     //    .attr('stroke-opacity', 0.7)
+		     //    .attr('fill', 'none');
+
+
 
 	        // add the entropy line
-	        focus.append('path')
+	        entropyPane.append('path')
 		        .datum(entropy)
-		        .attr('class', 'entropy')
+		        .attr('class', 'entropy line')
 		        .attr('stroke', 'green')
 		        .attr('stroke-width', '2px')
-		        .attr('stroke-opacity', 0.5)
+		        .attr('stroke-opacity', 0.7)
 		        .attr('fill', 'none')
 		        .attr('d', entropyLine);
 
 	        // container for the lines and associated elements
-	        var samples = focus.selectAll('.sample')
+	        var samples = sequencePane.selectAll('.sample')
 		        .data(data)
 	          .enter().append('g')
 		        .attr('class', 'sample');
@@ -201,13 +241,13 @@ function parallelCoordinates() {
 	        // add x axis to focus
 	        context.append('g')
 		        .attr('class', 'x axis')
-		        .attr('transform', 'translate(0,' + height2 + ')')
-		        .call(xAxis2);
+		        .attr('transform', 'translate(0,' + heightBottom + ')')
+		        .call(xAxisContext);
 
 	        context.append('g')
 		        .attr('class', 'brush')
 		        .call(brush)
-		        .call(brush.move, x.range());
+		        .call(brush.move, xTop.range());
 
 	        // add the entropy line
 	        context.append('path')
@@ -216,7 +256,7 @@ function parallelCoordinates() {
 		        .attr('stroke', 'green')
 		        .attr('stroke-width', '3px')
 		        .attr('fill', 'none')
-		        .attr('d', entropyLine2);
+		        .attr('d', entropyLineBottom);
 
 	        var samples2 = context.selectAll('.sample')
 		        .data(data)
@@ -226,7 +266,7 @@ function parallelCoordinates() {
 	        // add lines
 	        samples2.append('path')
 		        .attr('class', 'context line')
-		        .attr('d', line2)
+		        .attr('d', lineBottom)
 		        .style('stroke', colour || 'steelblue');
 	        //========================================================================
 
@@ -235,12 +275,9 @@ function parallelCoordinates() {
 	        // FUNCTIONS THAT CONTROL THE BRUSHING AND ZOOMING FUNCTIONALITY
 	        function brushed() {
 		        if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return; // ignore brush-by-zoom
-		        var s = d3.event.selection || x2.range();
-		        x.domain(s.map(x2.invert, x2));
-		        focus.selectAll('.line').attr('d', line);
-		        focus.selectAll('.entropy').attr('d', entropyLine);
-		        focus.select('.x.axis.top').call(xAxisTop);
-		        focus.select('.x.axis.bottom').call(xAxisBottom);
+		        var s = d3.event.selection || xBottom.range();
+		        xTop.domain(s.map(xBottom.invert, xBottom));
+		        updateChart();
 		        svg.select('.zoom').call(zoom.transform, d3.zoomIdentity
 			        .scale(width / (s[1] - s[0]))
 			        .translate(-s[0], 0));
@@ -249,12 +286,17 @@ function parallelCoordinates() {
 	        function zoomed() {
 		        if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'brush') return; // ignore zoom-by-brush
 		        var t = d3.event.transform;
-		        x.domain(t.rescaleX(x2).domain());
-		        focus.selectAll('.line').attr('d', line);
-		        focus.selectAll('.entropy').attr('d', entropyLine);
-		        focus.select('.x.axis.top').call(xAxisTop);
-		        focus.select('.x.axis.bottom').call(xAxisBottom);
-		        context.select('.brush').call(brush.move, x.range().map(t.invertX, t));
+		        xTop.domain(t.rescaleX(xBottom).domain());
+		        updateChart();
+		        context.select('.brush').call(brush.move, xTop.range().map(t.invertX, t));
+	        }
+
+	        function updateChart() {
+		        sequencePane.selectAll('.line').attr('d', line);
+		        entropyPane.select('.entropy.line').attr('d', entropyLine);
+		        sequencePane.select('.x.axis.top').call(xAxisTopSeq);
+		        sequencePane.select('.x.axis.bottom').call(xAxisBottomFocus);
+		        entropyPane.select('.x.axis.entropy').call(xAxisBottomFocus);
 	        }
 	        //========================================================================
         });
@@ -267,22 +309,34 @@ function parallelCoordinates() {
     chart.width = function(value) {
         if (!arguments.length) return width;
         totalWidth = value;
-        width = value - margin.left - margin.right;
+        width = value - marginTop.left - marginTop.right;
         return chart;
     };
 
     chart.height = function(value) {
-        if (!arguments.length) return height;
+        if (!arguments.length) return heightTop;
         totalHeight = value;
-        height = value - margin.top - margin.bottom;
+        heightTop = value - marginTop.top - marginTop.bottom;
         return chart;
     };
 
-    chart.margin = function(value) {
-        if (!arguments.length) return margin;
-        margin = value;
+    chart.marginTop = function(value) {
+        if (!arguments.length) return marginTop;
+        marginTop = value;
         return chart;
     };
+
+	chart.marginMiddle = function(value) {
+		if (!arguments.length) return marginMiddle;
+		marginMiddle = value;
+		return chart;
+	};
+
+	chart.marginBottom = function(value) {
+		if (!arguments.length) return marginBottom;
+		marginBottom = value;
+		return chart;
+	};
 
     chart.sequence = function(value) {
     	if (!arguments.length) return sequence;
@@ -365,7 +419,7 @@ function parallelCoordinates() {
 	};
 
 	// gridlines in y axis function
-	function make_y_gridlines() { return d3.axisLeft(y).ticks(5); }
+	function make_y_gridlines(scale) { return d3.axisLeft(scale).ticks(5); }
 
 	// calculate the shannon entropy (variance) for the data
 	function shannonEntropy(data) {
